37c10892d2dedf0dbda99e4c3b38d7b4c75a3db5
Christian Baverstock <baverstockchristian@gmail.com>
2012-08-20 22:28:22 +0100

    Added "players"

--- src/scripts.js
+++ src/scripts.js
@@ -1,22 +1,26 @@
+//Add some globals
 var timer = 0;
 var canvas;
 var context;
 
+//Use for game initialisation
 window.onload = function () {
    canvas = document.getElementById("gameCanvas");
    context = canvas.getContext("2d");
 
    drawGrid();
+   drawElements();
    start();
 }
 
-
+//Looping Game Turn function
 function start() {
     document.getElementById('timer').innerHTML = timer;
     timer++;
     setTimeout('start()', 50);
 }
 
+//Draw the grid
 function drawGrid() {
     for (i = 0; i < 10; i++) {
         context.beginPath();
@@ -30,7 +34,24 @@ function drawGrid() {
         context.lineTo(i*50,500);
         context.stroke();
     }
+    
 }
 
+//Draw "Players"
 function drawElements() {
+    context.beginPath();
+    context.rect(60, 360, 40, 40);
+    context.fillStyle = '#8ED6FF';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'black';
+    context.stroke();
+
+    context.beginPath();
+    context.rect(500, 360, 40, 40);
+    context.fillStyle = '#8ED6FF';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'black';
+    context.stroke();
 }
