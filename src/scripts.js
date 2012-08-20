446b8be924793f2e4e56be73fda3aae927d39d23
Christian Baverstock <baverstockchristian@gmail.com>
2012-08-20 21:18:35 +0100

    Added game timer

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,11 @@
-//javascript lol
+var timer = 0;
+
+function init() {
+    start();
+}
+
+function start() {
+    document.getElementById('timer').innerHTML = timer;
+    timer++;
+    setTimeout('start()', 50);
+}
